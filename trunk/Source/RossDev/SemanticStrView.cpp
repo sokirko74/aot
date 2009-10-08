d7af8b8fbafc2c5abebcfab8ef2c3331c1346904
sokirko <sokirko@a697401b-0683-4274-95e2-f182755d979b>
2009-10-08 15:18:42 +0000

    new vcproj

--- trunk/Source/RossDev/SemanticStrView.cpp
+++ trunk/Source/RossDev/SemanticStrView.cpp
@@ -248,10 +248,13 @@ LRESULT CSemanticStrView::WindowProc(UINT message, WPARAM wParam, LPARAM lParam)
 void CSemanticStrView::PasteClipboard() 
 {
 	OpenClipboard();
-    HGLOBAL hMem = ::GetClipboardData(CF_TEXT );
-    if (!hMem) {        CloseClipboard();        return;    }
+    HGLOBAL hMem = ::GetClipboardData(CF_UNICODETEXT );
+    if (!hMem) {        
+        CloseClipboard();        
+        return;    
+    }
 
-	char s  [5000];
+	wchar_t s  [5000];
 	if ( GlobalSize(hMem) > 4999) 
 	{
 		AfxMessageBox (" Text is too big!");
@@ -259,15 +262,15 @@ void CSemanticStrView::PasteClipboard()
 
 	CopyMemory( s, hMem, GlobalSize(hMem));
 	s[GlobalSize(hMem)] = 0;
-	//AfxMessageBox (s);
+    CString Q = s;
 	char cmd[5200];
 	strcpy (cmd, "$main.controls.mainEntry insert 0.0 \"");
-    for (size_t i=0; i < strlen  (s);i++)
+    for (size_t i=0; i < Q.GetLength();i++)
 		if (s[i] == '"')
 			strcat (cmd, "\\\"");
 		else
 		{   size_t l = strlen(cmd);
-			cmd[l] = s[i];
+			cmd[l] = Q[i];
 			cmd[l+1] = 0; };
 
     strcat (cmd, "\"");
