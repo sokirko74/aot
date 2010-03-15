2e63cd3d6f450ceebd282f41ce6301cb5bd9511c
sokirko <sokirko@a697401b-0683-4274-95e2-f182755d979b>
2010-03-15 16:32:29 +0000

    make output xml-like

--- trunk/Source/TestSynan/TestSynan.cpp
+++ trunk/Source/TestSynan/TestSynan.cpp
@@ -3,29 +3,39 @@
 #include "../common/SyntaxHolder.h"
 
 
-void GetAnanlytForms(const CSentence& Sentence, StringSet& Groups)
+void GetAnanlytForms(const CSentence& Sentence, string& Res)
 {
     for( int WordNo = 0; WordNo<Sentence.m_Words.size(); WordNo++ )
     {
        const CSynWord& W = Sentence.m_Words[WordNo];
        if (!W.m_MainVerbs.empty())
         {
-            string S = string("AnalytForm\t=\t")+  W.m_strWord.c_str();
+            Res += string("\t<analyt> ")+  W.m_strWord.c_str();
             for (size_t i=0; i< W.m_MainVerbs.size(); i++)
 		    {
-                S += string(" ") + Sentence.m_Words[W.m_MainVerbs[i]].m_strWord;
+                Res += string(" ") + Sentence.m_Words[W.m_MainVerbs[i]].m_strWord;
 
 			    const CSynWord& W_1 = Sentence.m_Words[W.m_MainVerbs[i]];
 			    for (size_t j=0; j< W_1.m_MainVerbs.size(); j++)
-				    S += string(" ") + Sentence.m_Words[W_1.m_MainVerbs[j]].m_strWord;
-
+				    Res += string(" ") + Sentence.m_Words[W_1.m_MainVerbs[j]].m_strWord;
 		    };
-		    Groups.insert(S);
+            Res += "</analyt>\n";
        }
     }
 }
+string GetWords(const CSentence& Sentence, const CPeriod& P)
+{
+    string S;
+    for (int WordNo = P.m_iFirstWord; WordNo <= P.m_iLastWord; WordNo++)
+    {
+        S += Sentence.m_Words[WordNo].m_strWord;
+        if (WordNo < P.m_iLastWord)
+            S += " ";
+    };
+    return S;
+}
 
-void GetGroups(const CSentence& Sentence, const CAgramtab& A, StringSet& Groups)
+void GetGroups(const CSentence& Sentence, const CAgramtab& A, string& Res)
 {
     int nClausesCount = Sentence.GetClausesCount();
 
@@ -42,40 +52,28 @@ void GetGroups(const CSentence& Sentence, const CAgramtab& A, StringSet& Groups)
             if( pSynVar->m_iWeight < nVmax ) break;
 
             const CMorphVariant& V = *pSynVar;
+            Res += Format("\t<synvar>\n");
 
             // print the clause
-            int ClauseType = (V.m_ClauseTypeNo == -1) ? UnknownSyntaxElement  :  Clause.m_vectorTypes[V.m_ClauseTypeNo].m_Type;;
-
-            string S;
-            if (ClauseType != UnknownSyntaxElement)
-                S = (const char*)A.GetClauseNameByType(ClauseType);
-            else
-                S = "EMPTY";
-
-            S += "\t=\t";
-            for (int WordNo = Clause.m_iFirstWord; WordNo <= Clause.m_iLastWord; WordNo++)
             {
-                S += Sentence.m_Words[WordNo].m_strWord;
-                S += " ";
-            };
-            Groups.insert(S);
-
-            // распечатываем все группы 
+                int ClauseType = (V.m_ClauseTypeNo == -1) ? UnknownSyntaxElement  :  Clause.m_vectorTypes[V.m_ClauseTypeNo].m_Type;;
+                string Type;
+                if (ClauseType != UnknownSyntaxElement)
+                    Type = (const char*)A.GetClauseNameByType(ClauseType);
+                else
+                    Type = "EMPTY";
+                Res += Format("\t\t<clause type=\"%s\">%s</clause>\n", Type.c_str(), GetWords(Sentence, Clause).c_str());
+            }
 
 
             for (int GroupNo = 0; GroupNo < V.m_vectorGroups.GetGroups().size(); GroupNo++)
             {
                 const CGroup& G  = V.m_vectorGroups.GetGroups()[GroupNo];
-
-                S = (const char*)Sentence.GetOpt()->GetGroupNameByIndex(G.m_GroupType);
-                S += "\t=\t";
-                for (int WordNo = G.m_iFirstWord; WordNo <= G.m_iLastWord; WordNo++)
-                {
-                    S += Sentence.m_Words[WordNo].m_strWord;
-                    S += " ";
-                };
-                Groups.insert(S);
+                Res += Format("\t\t<group type=\"%s\">%s</group>\n", 
+                    Sentence.GetOpt()->GetGroupNameByIndex(G.m_GroupType), 
+                    GetWords(Sentence, G).c_str());
             };
+            Res += Format("\t</synvar>\n");
         }
     }
 
@@ -84,22 +82,10 @@ void GetGroups(const CSentence& Sentence, const CAgramtab& A, StringSet& Groups)
 
 string GetNodeStr(const CSentence& Sentence, const CRelationsIterator& RelIt, int GroupNo, int WordNo)
 {
-    string Result;
     if (GroupNo != -1)
-    {
-        for (size_t  i=RelIt.GetFirmGroups()[GroupNo].m_iFirstWord; i <= RelIt.GetFirmGroups()[GroupNo].m_iLastWord; i++)
-        {
-            Result += Sentence.m_Words[i].m_strWord;
-            Result += " ";
-        }
-
-    }
+        return GetWords(Sentence, RelIt.GetFirmGroups()[GroupNo]);
     else
-    {
-        Result += Sentence.m_Words[WordNo].m_strWord;
-        Result += " ";
-    }
-    return Result;
+        return Sentence.m_Words[WordNo].m_strWord;
 }
 
 void GetRelations(const CSentence& Sentence, string& Result)
@@ -109,79 +95,43 @@ void GetRelations(const CSentence& Sentence, string& Result)
     for( int i = 0; i<Sentence.m_vectorPrClauseNo.size(); i++ )
 		RelIt.AddClauseNoAndVariantNo(Sentence.m_vectorPrClauseNo[i], 0);
     RelIt.BuildRelations();
-    StringSet Rels;
    	for(long RelNo = 0 ; RelNo < RelIt.GetRelations().size() ; RelNo++ )
 	{
 		const CSynOutputRelation& piRel = RelIt.GetRelations()[RelNo];
 		string RelName = Sentence.GetOpt()->GetGroupNameByIndex(piRel.m_Relation.type);
         string Src = GetNodeStr(Sentence, RelIt,piRel.m_iSourceGroup, piRel.m_Relation.m_iFirstWord);
         string Trg = GetNodeStr(Sentence, RelIt,piRel.m_iTargetGroup, piRel.m_Relation.m_iLastWord);
-        Rels.insert( Format(" %s ( %s-> %s)\n", RelName.c_str(), Src.c_str(),  Trg.c_str()) );
+        Result += Format("\t<rel name=\"%s\"> %s -> %s </rel>\n", RelName.c_str(), Src.c_str(),  Trg.c_str() );
     }
-    for (StringSet::const_iterator it = Rels.begin(); !(it == Rels.end()); it++)
-	    Result += *it;
-
-
-
 }
-string GetStringBySyntax(const CSentencesCollection& SC, const CAgramtab& A)
+
+string GetStringBySyntax(const CSentencesCollection& SC, const CAgramtab& A, string input)
 {
     string Result;
+    Result += Format("<chunk>\n");
+    Result += Format("<input>%s</input>\n",input.c_str());
     for (size_t nSent=0;  nSent < SC.m_vectorSents.size(); nSent++)
     {
-        if (SC.m_vectorSents.size() > 1)
-            Result += Format("SentNo=%i\n", nSent);
-
-
-
 	    const CSentence& Sentence = *SC.m_vectorSents[nSent];
 	    int iWord=0,iClau=0,iCvar=0;
-	    StringSet Groups;
-
-        GetAnanlytForms(Sentence, Groups);
-        GetGroups(Sentence, A, Groups);
-	    for (StringSet::const_iterator it = Groups.begin(); !(it == Groups.end()); it++)
-	    {
-		    Result += *it;
-		    Result += "\n";
-	    };
+        Result += "<sent>\n";
+        GetAnanlytForms(Sentence, Result);
+        GetGroups(Sentence, A, Result);
         GetRelations(Sentence, Result);
-
+        Result += "</sent>\n";
     }
+    Result += Format("</chunk>\n");
 
     fprintf (stderr, "sentences count: %i\n", SC.m_vectorSents.size());
 	return Result;
-
-	//return "ddd";
-
 };
 	
-extern void KOI8ToWin (string& s);
-extern void WinToKOI8 (string& s);
-void RussianConvertToScreen (string& S)
-{
-	#ifdef WIN32
-		CharToOemBuff((char*)S.c_str(), (char*)S.c_str(), S.length());		
-	#else
-		WinToKOI8(S);
-	#endif
-
-};
-void RussianConvertToDictionary (string& S)
-{
-	#ifdef WIN32
-		OemToCharBuff((char*)S.c_str(), (char*)S.c_str(), S.length());		
-	#else
-		KOI8ToWin(S);
-	#endif
-
-};
 
 
 void PrintUsage()
 {
 		printf ("Dialing DWDS Command Line Syntax Parser(www.aot.ru, www.dwds.de)\n");
-		printf ("Usage: TestSynan (RUSSIAN|GERMAN) [filename.txt]\n");
+		printf ("Usage: TestSynan (RUSSIAN|GERMAN) [filename.txt] - \n");
 		printf ("Example: TestSynan Russian\n");
 		exit(1);	
 };
@@ -192,20 +142,35 @@ int main(int argc, char* argv[])
 try {
 	
 
-	MorphLanguageEnum langua;
+	MorphLanguageEnum langua = morphUnknown;
     string  FileName;
-    if (argc == 3)
-    {
-        FileName = argv[2];
-        if (!FileExists(FileName.c_str()))
-        {
-            fprintf (stderr, "cannot open file %s\n",  FileName.c_str());
-            exit(1);
+	for (size_t i=1; i<argc; i++)
+	{
+		string s = argv[i];
+		EngMakeLower(s);
+		if (		(s == "-help") 
+				||	(s == "--help") 
+				||	(s == "/?") 
+			)
+			PrintUsage();
+        else {
+            if (langua == morphUnknown) {
+                if (!GetLanguageByString(s, langua)) 
+                        PrintUsage();
+            }
+            else {
+                FileName = s;
+                if (!FileExists(FileName.c_str()))
+                {
+                    fprintf (stderr, "cannot open file %s\n",  FileName.c_str());
+                    exit(1);
+                }
+            }
         }
-        argc--;
+
     }
-	if (argc != 2) PrintUsage();
-	if (!GetLanguageByString(argv[1], langua)) PrintUsage();
+	if ((langua == morphUnknown)) PrintUsage();
+
 	CSyntaxHolder H;
 	
 	if (!H.LoadSyntax(langua))
@@ -244,34 +209,24 @@ try {
             fflush (stderr);
             H.m_bTimeStatis = true;
             H.GetSentencesFromSynAn(Files[i], true);
-	    	string s = GetStringBySyntax(H.m_Synan, *H.m_pGramTab);
+	    	string s = GetStringBySyntax(H.m_Synan, *H.m_pGramTab, Files[i].c_str());
 		    printf  ("%s\n\n", s.c_str());
         }
         return 0;
     }
 
 
-	printf ("Input a sentence..\n");
 	// ===============  WORKING ===============
-	for (;;)
+    char buffer[10000] ;
+	while  (fgets(buffer, 10000, stdin))
 	{
-		printf (">");
-		char buffer[1000] ;
-		fgets(buffer, 1000, stdin);
-		if (strlen(buffer) == 0) break;
 		string s = buffer;
 		Trim(s);
-		if (s.empty()) break;
+		if (s.empty()) continue;
 		
-		RussianConvertToDictionary(s);
-		
-		//string s = "мама мыла раму";
-		//string s = "die schwache Resolution";
-		//string s = "test";
 		H.GetSentencesFromSynAn(s, false);
-		s = GetStringBySyntax(H.m_Synan, *H.m_pGramTab);
-		RussianConvertToScreen(s);	
-		printf  ("%s\n\n", s.c_str());
+		s = GetStringBySyntax(H.m_Synan, *H.m_pGramTab, s);
+		printf  ("%s", s.c_str());
         fflush(stdout);
 	};
 }
