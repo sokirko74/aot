bc8587ee8e2069dd5cac216c867db54d11be06db
Alexey Sokirko <sokirko@yandex.ru>
2010-09-06 13:34:05 +0000

    update vs

--- Source/Demo/StdAfx.h
+++ Source/Demo/StdAfx.h
@@ -10,7 +10,7 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#define _WIN32_WINNT 0x0400
+#define _WIN32_WINNT 0x0500
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>         // MFC core and standard components
