43a8d22f84008325605f0b006d772ac31eab82d4
l0pan <l0pan@a697401b-0683-4274-95e2-f182755d979b>
2012-07-30 13:38:42 +0000

    Fix for numbers in FIO

--- trunk/Source/MAPostLib/Fio.cpp
+++ trunk/Source/MAPostLib/Fio.cpp
@@ -18,44 +18,44 @@ struct CFIOItem {
 };
 
 
-struct CFIOFormat
+struct CFIOFormat 
 {
 	string m_FormatStr;
 	bool   m_GleicheCase;
 	vector<CFIOItem> m_Items;
 	CFIOFormat () {};
 
-	CFIOFormat (string  FormatStr, bool GleicheCase)
+	CFIOFormat (string  FormatStr, bool GleicheCase) 
 	{
 		m_FormatStr = FormatStr;
 		m_GleicheCase = GleicheCase;
 		StringTokenizer tok(FormatStr.c_str(), " ");
-		while (tok())
+		while (tok()) 
 		{
 			string s = tok.val();
 			FioItemTypeEnum t;
-		    if  (s == "")
+		    if  (s == "ИМЯ")
 				t = fiName;
 			else
-			if (s == "")
+			if (s == "ФАМИЛИЯ")
 				t = fiSurname;
 			else
-			if (s == "")
+			if (s == "ОТЧЕСТВО")
 				t = fiMiddle;
 		    else
-			if (s == "")
-				t = fiAbbr;
+			if (s == "ИНИЦИАЛ")
+				t = fiAbbr;	
 			else
-			if (s == "")
+			if (s == "ТОЧКА")
 				t = fiStop;
 		    else
-			if (s == "_")
+			if (s == "РИМСК_ЦК")
 				t = fiRoman;
 			else
-		 	if (s == "?")
+		 	if (s == "ИМ?")
 				t = fiProbName;
 			else
-		 	if (s == "_")
+		 	if (s == "ПОРЯД_ЧИСЛ")
 				t = fiOrdinal;
 		    else
 			{
@@ -65,16 +65,16 @@ struct CFIOFormat
 			I.m_fiType = t;
 			I.m_ItemStr = s;
 			m_Items.push_back(I);
-		};
+		};	
 	};
 };
 
 
-static void GetCommonVariants(const vector<SmallHomonymsVec>&  Parents,
+static void GetCommonVariants(const vector<SmallHomonymsVec>&  Parents, 
 									   SmallHomonymsVec&   V,
-							  vector<SmallHomonymsVec>&  Variants,
+							  vector<SmallHomonymsVec>&  Variants, 
 								long       Position)
-{
+{ 
     if (Variants.size() > 1000) return;
 	if (Position < V.size())
 	for (long i=0; i< Parents[Position].size(); i++)
@@ -88,7 +88,7 @@ static void GetCommonVariants(const vector<SmallHomonymsVec>&  Parents,
 };
 
 
-static bool IsPartFio(const CMAPost& C, const CFIOItem& I, const CPostLemWord& Word, const CHomonym* pH)
+static bool IsPartFio(const CMAPost& C, const CFIOItem& I, const CPostLemWord& Word, const CHomonym* pH)  
 {
 
     if ( Word.HasDes(OFAM1) || Word.HasDes(OFAM2)  )    return false;
@@ -105,10 +105,10 @@ static bool IsPartFio(const CMAPost& C, const CFIOItem& I, const CPostLemWord& W
 
         case fiStop     :  return Word.m_strWord == ".";
         case fiRoman    :  return Word.HasDes(ORoman);
-		case fiOrdinal  :  return pH->HasPos(NUMERAL_P) && (unsigned char)Word.m_strWord[0]<224; // " "  " "
+		case fiOrdinal  :  return pH->HasPos(NUMERAL_P) && (unsigned char)Word.m_strWord[0]<224; //отбрасываем "Петр первый" вместо "Петр Первый"
         case fiProbName :  return Word.HasDes(ONam);
         default         :  return pH->m_strLemma == I.m_ItemStr;
-    }
+    }                
 
 
 };
@@ -119,23 +119,23 @@ bool IsPartOfNonSingleOborot(const CHomonym* pH)
     return pH->m_bOborot1 != pH->m_bOborot2;
 }
 
-static bool SetFioFormat (CMAPost& C, const CFIOFormat& Format, CLineIter it)
+static bool SetFioFormat (CMAPost& C, const CFIOFormat& Format, CLineIter it) 
 {
    vector<SmallHomonymsVec> Hypots;
    SmallWordsVec FioWords;
 
    Hypots.resize(Format.m_Items.size());
-
+   
    int CountOfVariants = 1;
    for (long ItemNo = 0; ItemNo < Format.m_Items.size() && it != C.m_Words.end(); ItemNo++, it=C.NextNotSpace(it))
    {
         FioWords.Add(it);
 		CPostLemWord& W = *it;
-
+        
         for (int HomNo=0; HomNo < W.GetHomonymsCount();  HomNo++)
 		{
             CHomonym* pH = W.GetHomonym(HomNo);
-			//  ".. "  
+			// иначе "Т.Е. ОТКАЗАТЬСЯ" будет ФИО
             if (IsPartOfNonSingleOborot(pH)) return false;
 
 			if (IsPartFio(C, Format.m_Items[ItemNo], W, pH))
@@ -145,9 +145,9 @@ static bool SetFioFormat (CMAPost& C, const CFIOFormat& Format, CLineIter it)
 		CountOfVariants *= Hypots[ItemNo].size();
    };
 
-   if (FioWords.size() != Format.m_Items.size()) return false; //  
+   if (FioWords.size() != Format.m_Items.size()) return false; // не достроилось
 
-   SmallHomonymsVec V; //  
+   SmallHomonymsVec V; // текущий вариант 
    vector<SmallHomonymsVec> Variants;
    Variants.reserve(CountOfVariants);
    V.m_ItemsCount = Hypots.size();
@@ -177,20 +177,20 @@ static bool SetFioFormat (CMAPost& C, const CFIOFormat& Format, CLineIter it)
        CHomonym* pH = Variants[0].m_Items[i];
        W.SetHomonymsDel(true);
        pH->m_bDelete = false;
-       pH->DeleteOborotMarks(); //    (    )
+       pH->DeleteOborotMarks(); // удаляем однословные оборотыб (многословных там быть не может)
        W.DeleteMarkedHomonymsBeforeClauses();
        if (W.HasDes(OSentEnd))
        {
            /*
-                 ,       
-	          ,    SENT_END    .
+             если ФИО содержало конец предложения, а после ФИО нет ни одной пометы 
+	         конца предложения, тогда надо поставить SENT_END на последнюю строчку ФИО.
            */
            W.DelDes(OSentEnd);
            FioWords.back()->AddDes(OSentEnd);
        }
    }
 
-   //  . 
+   // ставим графем. пометы
    FioWords[0]->AddDes(OFAM1);
    FioWords.back()->AddDes(OFAM2);
 
@@ -200,34 +200,34 @@ static bool SetFioFormat (CMAPost& C, const CFIOFormat& Format, CLineIter it)
 
 
 /*
-  ,
-  ,
- ,
- ,
- ,
-  ,
-  II
-.
+Петр Владимирович Иванов,
+Иванов Петр Владимирович,
+Петр Иванов,
+Иванов Петр,
+Петр Владимирович,
+Райнер Мария Рильке,
+Иоанн Павел II
+М.Горбачев
 */
 
-void CMAPost::Rule_Fio()
+void CMAPost::Rule_Fio() 
 {
     vector<CFIOFormat> FioFormats;
-    FioFormats.push_back(CFIOFormat("  ", true));
-    FioFormats.push_back(CFIOFormat("  ", true));
-    FioFormats.push_back(CFIOFormat(" ", true));
-    FioFormats.push_back(CFIOFormat(" ", true));
-    FioFormats.push_back(CFIOFormat(" ", true));
-    FioFormats.push_back(CFIOFormat("  ?", false)); //   
-    FioFormats.push_back(CFIOFormat("    ", false)); // .
-    FioFormats.push_back(CFIOFormat("  ", false)); // .
-    FioFormats.push_back(CFIOFormat("  _", false)); //   II
-    FioFormats.push_back(CFIOFormat(" _", false)); //  II
-    FioFormats.push_back(CFIOFormat(" _", true)); //  
-    FioFormats.push_back(CFIOFormat("  _", false)); //   
-    FioFormats.push_back(CFIOFormat(" ", false)); //  
+    FioFormats.push_back(CFIOFormat("ИМЯ ОТЧЕСТВО ФАМИЛИЯ", true));
+    FioFormats.push_back(CFIOFormat("ФАМИЛИЯ ИМЯ ОТЧЕСТВО", true));
+    FioFormats.push_back(CFIOFormat("ИМЯ ФАМИЛИЯ", true));
+    FioFormats.push_back(CFIOFormat("ФАМИЛИЯ ИМЯ", true));
+    FioFormats.push_back(CFIOFormat("ИМЯ ОТЧЕСТВО", true));
+    FioFormats.push_back(CFIOFormat("ИМЯ ИМЯ ИМ?", false)); // Эрих Мария Ремарк
+    FioFormats.push_back(CFIOFormat("ИНИЦИАЛ ТОЧКА ИНИЦИАЛ ТОЧКА ФАМИЛИЯ", false)); // М.Горбачев
+    FioFormats.push_back(CFIOFormat("ИНИЦИАЛ ТОЧКА ФАМИЛИЯ", false)); // М.Горбачев
+    FioFormats.push_back(CFIOFormat("ИМЯ ИМЯ РИМСК_ЦК", false)); // Иоанн Павел II
+    FioFormats.push_back(CFIOFormat("ИМЯ РИМСК_ЦК", false)); // Александр II
+    FioFormats.push_back(CFIOFormat("ИМЯ ПОРЯД_ЧИСЛ", true)); // Александр Второй
+    FioFormats.push_back(CFIOFormat("ИМЯ ИМЯ ПОРЯД_ЧИСЛ", false)); // Иоанн Павел Второй
+    FioFormats.push_back(CFIOFormat("ДОН ЖУАН", false)); // Дон Жуан
 
-
+    
     for (CLineIter it=m_Words.begin(); it !=  m_Words.end(); it++)
     {
         for (long FormatNo=0; FormatNo < FioFormats.size(); FormatNo++)
