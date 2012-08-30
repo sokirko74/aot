0290fc03c938cb50638947bbc0def8557cbd215f
Alexey Sokirko <sokirko@yandex.ru>
2012-08-30 13:19:56 +0000

    use pcre internally

---
+++ Bin/dll_reg_dbg.bat
@@ -0,0 +1,7 @@
+regsvr32 C:\RML\source\Agramtab\Debug\agramtab.dll
+regsvr32 /s C:\RML\source\Graphan\Debug\graphan.dll
+regsvr32 /s C:\RML\source\Lemmatizer\Debug\Lemmatizer.dll
+regsvr32 /s C:\RML\source\MAPost\Debug\MAPost.dll
+regsvr32 /s C:\RML\source\seman\Debug\seman.dll
+regsvr32 /s C:\RML\source\structDict\Debug\structDict.dll
+regsvr32 /s C:\RML\source\synAn\Debug\synAn.dll
