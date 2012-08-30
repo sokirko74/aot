aeef0f1faa5b9df09cef2304aa5155ee673a8cb1
sokirko <sokirko@a697401b-0683-4274-95e2-f182755d979b>
2012-08-30 12:16:09 +0000

    use pcre internally

--- trunk/Source/RossDev/SizeFormView.cpp
+++ trunk/Source/RossDev/SizeFormView.cpp
@@ -5,7 +5,7 @@
 //#include <afx.h>
 //#include <afxwin.h>
 //#include <..\mfc\src\afximpl.h>
-#include <..\atlmfc\src\mfc\afximpl.h>
+#include <../atlmfc/src/mfc/afximpl.h>
 #include "SizeFormView.h"
 
 #ifdef _DEBUG
@@ -20,26 +20,26 @@ static char THIS_FILE[] = __FILE__;
 IMPLEMENT_DYNAMIC(CSizeFormView, CFormView)
 
 CSizeFormView::CSizeFormView( LPCTSTR lpszTemplateName ) :
-		CFormView(lpszTemplateName)
+        CFormView(lpszTemplateName)
 {
-	m_hGadgetResize = NULL;
+    m_hGadgetResize = NULL;
 }
 
 CSizeFormView::CSizeFormView( UINT nIDTemplate ) :
-		CFormView(nIDTemplate)
+        CFormView(nIDTemplate)
 {
-	m_hGadgetResize = NULL;
+    m_hGadgetResize = NULL;
 }
 
 CSizeFormView::~CSizeFormView()
 {
-	DestroyGadgetResizeHandle(m_hGadgetResize);
+    DestroyGadgetResizeHandle(m_hGadgetResize);
 }
 
 BEGIN_MESSAGE_MAP(CSizeFormView, CFormView)
-	//{{AFX_MSG_MAP(CSizeFormView)
-	ON_WM_SIZE()
-	//}}AFX_MSG_MAP
+    //{{AFX_MSG_MAP(CSizeFormView)
+    ON_WM_SIZE()
+    //}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -48,77 +48,14 @@ END_MESSAGE_MAP()
 #ifdef _DEBUG
 void CSizeFormView::AssertValid() const
 {
-	CFormView::AssertValid();
+    CFormView::AssertValid();
 }
 
 void CSizeFormView::Dump(CDumpContext& dc) const
 {
-	CFormView::Dump(dc);
+    CFormView::Dump(dc);
 }
 
-BOOL AFXAPI _AfxCheckDialogTemplate(LPCTSTR lpszResource, BOOL bInvisibleChild)
-{
-	ASSERT(lpszResource != NULL);
-	HINSTANCE hInst = AfxFindResourceHandle(lpszResource, RT_DIALOG);
-	HRSRC hResource = ::FindResource(hInst, lpszResource, RT_DIALOG);
-	if (hResource == NULL)
-	{
-		if (HIWORD(lpszResource) != 0)
-			TRACE1("ERROR: Cannot find dialog template named '%s'.\n",
-				lpszResource);
-		else
-		{
-			DWORD Error = GetLastError();
-			TRACE1("ERROR: Cannot find dialog template with IDD 0x%04X.\n",
-				LOWORD((DWORD)lpszResource));
-		};
-		return FALSE;
-	}
-
-	if (!bInvisibleChild)
-		return TRUE;        // that's all we need to check
-
-	// we must check that the dialog template is for an invisible child
-	//  window that can be used for a form-view or dialog-bar
-	HGLOBAL hTemplate = LoadResource(hInst, hResource);
-	if (hTemplate == NULL)
-	{
-		TRACE0("Warning: LoadResource failed for dialog template.\n");
-		// this is only a warning, the real call to CreateDialog will fail
-		return TRUE;        // not a program error - just out of memory
-	}
-	DLGTEMPLATEEX* pTemplate = (DLGTEMPLATEEX*)LockResource(hTemplate);
-	DWORD dwStyle;
-	if (pTemplate->signature == 0xFFFF)
-		dwStyle = pTemplate->style;
-	else
-		dwStyle = ((DLGTEMPLATE*)pTemplate)->style;
-	UnlockResource(hTemplate);
-	FreeResource(hTemplate);
-
-	if (dwStyle & WS_VISIBLE)
-	{
-		if (HIWORD(lpszResource) != 0)
-			TRACE1("ERROR: Dialog named '%s' must be invisible.\n",
-				lpszResource);
-		else
-			TRACE1("ERROR: Dialog with IDD 0x%04X must be invisible.\n",
-				LOWORD((DWORD)lpszResource));
-		return FALSE;
-	}
-	if (!(dwStyle & WS_CHILD))
-	{
-		if (HIWORD(lpszResource) != 0)
-			TRACE1("ERROR: Dialog named '%s' must have the child style.\n",
-				lpszResource);
-		else
-			TRACE1("ERROR: Dialog with IDD 0x%04X must have the child style.\n",
-				LOWORD((DWORD)lpszResource));
-		return FALSE;
-	}
-
-	return TRUE;
-}
 
 #endif //_DEBUG
 
@@ -126,78 +63,67 @@ BOOL AFXAPI _AfxCheckDialogTemplate(LPCTSTR lpszResource, BOOL bInvisibleChild)
 // CSizeFormView message handlers
 
 BOOL CSizeFormView::Create(LPCTSTR /*lpszClassName*/, LPCTSTR /*lpszWindowName*/,
-	DWORD dwRequestedStyle, const RECT& rect, CWnd* pParentWnd, UINT nID,
-	CCreateContext* pContext)
+    DWORD dwRequestedStyle, const RECT& rect, CWnd* pParentWnd, UINT nID,
+    CCreateContext* pContext)
 {
-	ASSERT(pParentWnd != NULL);
-	ASSERT(m_lpszTemplateName != NULL);
+    ASSERT(pParentWnd != NULL);
+    ASSERT(m_lpszTemplateName != NULL);
 
-	m_pCreateContext = pContext;    // save state for later OnCreate
+    m_pCreateContext = pContext;    // save state for later OnCreate
 
-	
-#ifdef _DEBUG
-	// dialog template must exist and be invisible with WS_CHILD set
-	if (!_AfxCheckDialogTemplate(m_lpszTemplateName, TRUE))
-	{
-		ASSERT(FALSE);          // invalid dialog template name
-		PostNcDestroy();        // cleanup if Create fails too soon
-		return FALSE;
-	}
-#endif //_DEBUG
+    // initialize common controls
+    //VERIFY(AfxDeferRegisterClass(AFX_WNDCOMMCTLS_REG));
+    AfxDeferRegisterClass(AFX_WNDCOMMCTLSNEW_REG);
 
-	// initialize common controls
-	VERIFY(AfxDeferRegisterClass(AFX_WNDCOMMCTLS_REG));
-	AfxDeferRegisterClass(AFX_WNDCOMMCTLSNEW_REG);
+    // call PreCreateWindow to get prefered extended style
+    CREATESTRUCT cs; memset(&cs, 0, sizeof(CREATESTRUCT));
+    if (dwRequestedStyle == 0)
+        dwRequestedStyle = AFX_WS_DEFAULT_VIEW;
+    cs.style = dwRequestedStyle;
+    if (!PreCreateWindow(cs))
+        return FALSE;
 
-	// call PreCreateWindow to get prefered extended style
-	CREATESTRUCT cs; memset(&cs, 0, sizeof(CREATESTRUCT));
-	if (dwRequestedStyle == 0)
-		dwRequestedStyle = AFX_WS_DEFAULT_VIEW;
-	cs.style = dwRequestedStyle;
-	if (!PreCreateWindow(cs))
-		return FALSE;
+    // create a modeless dialog
+    if (!CreateDlg(m_lpszTemplateName, pParentWnd))
+        return FALSE;
 
-	// create a modeless dialog
-	if (!CreateDlg(m_lpszTemplateName, pParentWnd))
-		return FALSE;
+    m_pCreateContext = NULL;
 
-	m_pCreateContext = NULL;
+    // we use the style from the template - but make sure that
+    //  the WS_BORDER bit is correct
+    // the WS_BORDER bit will be whatever is in dwRequestedStyle
+    ModifyStyle(WS_BORDER|WS_CAPTION, cs.style & (WS_BORDER|WS_CAPTION));
+    ModifyStyleEx(WS_EX_CLIENTEDGE, cs.dwExStyle & WS_EX_CLIENTEDGE);
 
-	// we use the style from the template - but make sure that
-	//  the WS_BORDER bit is correct
-	// the WS_BORDER bit will be whatever is in dwRequestedStyle
-	ModifyStyle(WS_BORDER|WS_CAPTION, cs.style & (WS_BORDER|WS_CAPTION));
-	ModifyStyleEx(WS_EX_CLIENTEDGE, cs.dwExStyle & WS_EX_CLIENTEDGE);
+    SetDlgCtrlID(nID);
 
-	SetDlgCtrlID(nID);
+    m_hGadgetResize = CreateGadgetResizeHandle(this);
 
-	m_hGadgetResize = CreateGadgetResizeHandle(this);
+    CRect rectTemplate;
+    GetWindowRect(rectTemplate);
+    SetScaleToFitSize(rectTemplate.Size());
 
-	CRect rectTemplate;
-	GetWindowRect(rectTemplate);
-	SetScaleToFitSize(rectTemplate.Size());
+    // initialize controls etc
+    if (!ExecuteDlgInit(m_lpszTemplateName))
+        return FALSE;
 
-	// initialize controls etc
-	if (!ExecuteDlgInit(m_lpszTemplateName))
-		return FALSE;
+    // force the size requested
+    SetWindowPos(NULL, rect.left, rect.top,
+        rect.right - rect.left, rect.bottom - rect.top,
+        SWP_NOZORDER|SWP_NOACTIVATE);
 
-	// force the size requested
-	SetWindowPos(NULL, rect.left, rect.top,
-		rect.right - rect.left, rect.bottom - rect.top,
-		SWP_NOZORDER|SWP_NOACTIVATE);
+    // make visible if requested`
+    if (dwRequestedStyle & WS_VISIBLE)
+        ShowWindow(SW_NORMAL);
 
-	// make visible if requested`
-	if (dwRequestedStyle & WS_VISIBLE)
-		ShowWindow(SW_NORMAL);
-
-	return TRUE;
+    return TRUE;
 }
 
-void CSizeFormView::OnSize(UINT nType, int cx, int cy) 
+void CSizeFormView::OnSize(UINT nType, int cx, int cy)
 {
-	if (m_hGadgetResize)
-		ResizeGadgetsOnWindow(m_hGadgetResize, cx, cy);
-	else
-		CFormView::OnSize(nType, cx, cy);
+    if (m_hGadgetResize)
+        ResizeGadgetsOnWindow(m_hGadgetResize, cx, cy);
+    else
+        CFormView::OnSize(nType, cx, cy);
 }
 
--- trunk/Source/RossDev/StdAfx.h
+++ trunk/Source/RossDev/StdAfx.h
@@ -6,13 +6,28 @@
 #if !defined(rosssdev_stadafx)
 #define rosssdev_stadafx
 
+#ifndef WINVER                          // Specifies that the minimum required platform is Windows Vista.
+#define WINVER 0x0501           // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT            // Specifies that the minimum required platform is Windows Vista.
+#define _WIN32_WINNT 0x0600     // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINDOWS          // Specifies that the minimum required platform is Windows 98.
+#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE                       // Specifies that the minimum required platform is Internet Explorer 7.0.
+#define _WIN32_IE 0x0700        // Change this to the appropriate value to target other versions of IE.
+#endif
+
 #if _MSC_VER > 1000
 #pragma once
 #endif // _MSC_VER > 1000
 #pragma warning (disable : 4786)
 
 #define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
-#define _WIN32_WINNT 0x0500
 
 #include "resource.h"
 
