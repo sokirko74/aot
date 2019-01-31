3c7d981577d61eb63d8bc37db5b51814f82af5f9
sokirko <sokirko@a697401b-0683-4274-95e2-f182755d979b>
2019-01-31 14:12:59 +0000

    add website scripts

---
+++ trunk/Bin/run_seman.sh
@@ -0,0 +1,5 @@
+while [ 1 ]; do
+    echo "start semantics"
+    $RML/Bin/SemanDaemon start_cli
+    sleep 5
+done
---
+++ trunk/Bin/start_aot.sh
@@ -0,0 +1,3 @@
+﻿nohup bash run_seman.sh > run_seman.log &
+nohup bash run_synan.sh  > run_synan.log &
+nohup bash run_concord.sh > run_concord.log&
