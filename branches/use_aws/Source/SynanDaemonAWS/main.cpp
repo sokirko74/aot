53b9c3bb09e88e537dadbb34e0c386b2a2ebc1be
sokirko <sokirko@a697401b-0683-4274-95e2-f182755d979b>
2020-02-15 10:23:03 +0000

    try use aws buckets, but it does not work

--- branches/use_aws/Source/SynanDaemonAWS/main.cpp
+++ branches/use_aws/Source/SynanDaemonAWS/main.cpp
@@ -3,11 +3,23 @@
 // "Arn": "arn:aws:iam::516891605423:role/synan-daemon-invoker-role",
 // $ make -C ~/RML/cmake-build-debug/ aws-lambda-package-SynanDaemonAWS
 // aws lambda delete-function --function-name invoke-synan
-// aws lambda create-function --function-name invoke-synan --role arn:aws:iam::516891605423:role/synan-daemon-invoker-role --runtime provided --timeout 15 --memory-size 128 --handler synan-daemon --zip-file fileb:///home/sokirko/RML/cmake-build-debug/Source/SynanDaemonAWS/SynanDaemonAWS.zip
-// aws lambda invoke --function-name hello-world --payload '{ }' output.txt
+// aws lambda create-function --function-name invoke-synan --role arn:aws:iam::516891605423:role/synan-daemon-invoker-role --runtime provided --timeout 15 --memory-size 256 --handler synan-daemon --zip-file fileb:///home/sokirko/RML/cmake-build-debug/Source/SynanDaemonAWS/SynanDaemonAWS.zip
+// aws lambda invoke --function-name invoke-synan  --payload '{"action":"morph" }' output.txt
 
-#include <aws/lambda-runtime/runtime.h>
+#include <aws/core/Aws.h>
+#include <aws/core/utils/logging/LogLevel.h>
+#include <aws/core/utils/logging/ConsoleLogSystem.h>
+#include <aws/core/utils/logging/LogMacros.h>
 #include <aws/core/utils/json/JsonSerializer.h>
+#include <aws/core/utils/HashingUtils.h>
+#include <aws/core/platform/Environment.h>
+#include <aws/core/client/ClientConfiguration.h>
+#include <aws/core/auth/AWSCredentialsProvider.h>
+#include <aws/s3/S3Client.h>
+#include <aws/s3/model/GetObjectRequest.h>
+#include <aws/lambda-runtime/runtime.h>
+#include <iostream>
+#include <fstream>
 
 using namespace aws::lambda_runtime;
 
@@ -23,8 +35,45 @@ invocation_response my_handler(invocation_request const& request)
     return invocation_response::success(action, "application/json");
 }
 
+char const TAG[] = "LAMBDA_ALLOC";
+
+void download_and_save_file(
+        Aws::S3::S3Client const& client,
+        Aws::String const& bucket,
+        Aws::String const& key,
+        const char* fileName)
+{
+    using namespace Aws;
+    AWS_LOGSTREAM_INFO(TAG, "try download dicts");
+
+    S3::Model::GetObjectRequest request;
+    request.WithBucket(bucket).WithKey(key);
+
+    auto outcome = client.GetObject(request);
+    if (outcome.IsSuccess()) {
+        AWS_LOGSTREAM_INFO(TAG, "Download completed!");
+        auto &retrieved_file = outcome.GetResultWithOwnership().GetBody();
+
+        std::ofstream output_file(fileName, std::ios::binary);
+        output_file << retrieved_file.rdbuf();
+    }
+    else {
+        AWS_LOGSTREAM_ERROR(TAG, "Failed with error: " << outcome.GetError());
+        return;
+    }
+}
+
 int main()
 {
-   run_handler(my_handler);
-   return 0;
+    using namespace Aws;
+    Aws::Client::ClientConfiguration config;
+    config.region = Aws::Environment::GetEnv("AWS_REGION");
+    config.caFile = "/etc/pki/tls/certs/ca-bundle.crt";
+
+    auto credentialsProvider = Aws::MakeShared<Aws::Auth::EnvironmentAWSCredentialsProvider>(TAG);
+    S3::S3Client client(credentialsProvider, config);
+    download_and_save_file(client, "morph.aot.ru", "morph.tar.gz", "/tmp/morph.tar.gz");
+
+    run_handler(my_handler);
+    return 0;
 }
