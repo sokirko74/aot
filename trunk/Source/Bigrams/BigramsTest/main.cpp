055eb7593da09eb2d8334f11fd1e158dc89ce902
sokirko <sokirko@a697401b-0683-4274-95e2-f182755d979b>
2020-02-16 15:24:28 +0000

    add tests for bigrams

---
+++ trunk/Source/Bigrams/BigramsTest/main.cpp
@@ -0,0 +1,38 @@
+#include <common/util_classes.h>
+#include <common/argparse.h>
+#include <common/BigramsReader.h>
+
+
+void initArgParser(int argc, const char **argv, ArgumentParser& parser) {
+    parser.AddOption("--help");
+    parser.AddArgument("--bigrams", "bigrams file with indices");
+    parser.AddArgument("--input-word", "input file in utf8");
+    parser.AddArgument("--language", "language");
+    parser.AddArgument("--output", "output");
+    parser.AddOption("--indirect");
+    parser.Parse(argc, argv);
+}
+
+int main(int argc, const char **argv) {
+    ArgumentParser args;
+    initArgParser(argc, argv, args);
+
+    string fileName = args.Retrieve("bigrams");
+    if (!FileExists(fileName.c_str()))
+        throw CExpc(Format("cannot find bigrams file: %s", fileName));
+    if (!InitializeBigrams(fileName))
+        throw CExpc(Format("cannot init bigrams"));
+
+    MorphLanguageEnum  language = args.GetLanguage();
+    bool direct  = ! args.Exists("indirect");
+    string word = args.Retrieve("input-word");
+    EngRusMakeUpper(word);
+    string result = GetConnectedWords(word, 0, direct, "SortByBigramsFreq", language );
+    args.GetOutputStream() << result << "\n";
+    return 0;
+}
+
+
+
+
+
--- trunk/Source/common/BigramsReader.h
+++ trunk/Source/common/BigramsReader.h
@@ -1,3 +1,31 @@
 #include "../common/utilit.h"
+#include "../common/bserialize.h"
+
+struct CBigramInfo {
+    DWORD WordIndex;
+    DWORD BigramsFreq;
+};
+
+inline size_t get_size_in_bytes (const CBigramInfo& t)
+{
+    return get_size_in_bytes(t.WordIndex) + get_size_in_bytes(t.BigramsFreq);
+
+};
+
+inline size_t save_to_bytes(const CBigramInfo& i, BYTE* buf)
+{
+    buf += save_to_bytes(i.WordIndex, buf);
+    buf += save_to_bytes(i.BigramsFreq, buf);
+    return get_size_in_bytes(i);
+}
+
+inline size_t restore_from_bytes(CBigramInfo& i, const BYTE* buf)
+{
+    buf += restore_from_bytes(i.WordIndex, buf);
+    buf += restore_from_bytes(i.BigramsFreq, buf);
+    return get_size_in_bytes(i);
+}
+
+
 extern string GetConnectedWords(string Word, int MinBigramsFreq, bool bDirectFile, string sortMode, MorphLanguageEnum langua);
 extern bool InitializeBigrams(string FileName);
