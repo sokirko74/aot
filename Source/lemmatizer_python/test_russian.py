2d7d30f8a4d9aa41f9584d204a2681e9984417b3
Alexey Sokirko <sokirko@yandex.ru>
2020-11-02 10:29:27 +0300

    use latin constants for Russian language

--- Source/lemmatizer_python/test_russian.py
+++ Source/lemmatizer_python/test_russian.py
@@ -6,5 +6,5 @@ import aot
 if __name__ == '__main__':
     print (aot.load_morphology(1))
 
-#    print (aot.lemmatize_json("мама", True))
+    print (aot.lemmatize_json("мама", True))
     print (aot.synthesize("мама", "N fem,sg,gen"))
