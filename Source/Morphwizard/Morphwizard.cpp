105424685e6bc7cb18cf7c78e4f2927fd7bb5408
Alexey Sokirko <sokirko@yandex.ru>
2021-01-06 20:19:00 +0300

    delete manifest file

--- Source/Morphwizard/Morphwizard.cpp
+++ Source/Morphwizard/Morphwizard.cpp
@@ -66,7 +66,7 @@ BOOL CMorphwizardApp::InitInstance()
 	// InitCommonControls() is required on Windows XP if an application
 	// manifest specifies use of ComCtl32.dll version 6 or later to enable
 	// visual styles.  Otherwise, any window creation will fail.
-	InitCommonControls();
+	//InitCommonControls();
 
 
 	AfxInitRichEdit2 ();
