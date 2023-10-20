67e753240595d46959bc1ab6b210faa59eeabf49
sokirko74 <sokirko@yandex.ru>
2023-10-20 12:02:28 +0300

    delete MorphXmlToken

--- Source/seman/SemanLib/stdafx.h
+++ Source/seman/SemanLib/stdafx.h
@@ -1,12 +1,4 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
-#ifndef  seman_linb_stadafx_h
-#define seman_lib_stadafx_h
-
-
+#pragma once
 
 
 #include "morph_dict/common/utilit.h"
@@ -16,5 +8,3 @@
 #include "graphan/GraphanLib/GraphanDicts.h"
 #include "morph_dict/AgramtabLib/rus_consts.h"
 #include "morph_dict/AgramtabLib/eng_consts.h"
-
-#endif
